08ed6908dc96216cf7d99a473e7e6e80d5a4e617
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-06 18:13:35 +0300

    tests on claim isolation across users

--- src/test/claimRegistration.ts
+++ src/test/claimRegistration.ts
@@ -26,6 +26,22 @@ function assertClaim(tenantId: string, userId: string, claim: data.Claim) {
     });
 }
 
+function assertNoClaim(tenantId: string, userId: string, claim: data.Claim) {
+    it('Should not list claim using admin api with admin user', async () => {
+        let http = await api.defaultAdminClient();
+        let response = await http.getJson(urls.adminClaims(tenantId, userId));
+        httpAssert.expectStatusCode(response, 200);
+        let claims = response.body;
+        expect(claims).to.eql([]);
+    });
+
+    it('Should not get claim by key using admin api with admin user', async () => {
+        let http = await api.defaultAdminClient();
+        let response = await http.getJson(urls.adminClaim(tenantId, userId, claim.key));
+        httpAssert.expectStatusCode(response, 404);
+    });
+}
+
 describe('Claim registration', () => {
     const tenant = data.randomTenant();
     const user = data.randomUser();
@@ -98,6 +114,119 @@ describe('Claim registration', () => {
             let response = await http.getJson(urls.adminClaim(tenant.id, user.id, deleted.key));
             httpAssert.expectStatusCode(response, 404);
         });
+    });
+
+    describe('After registering claims for different users', () => {
+        const user2 = data.randomUser();
+        const claim2 = data.randomClaim();
+        const claim3 = data.randomClaim();
+
+        before(async () => {
+            await api.dropDatabase();
+            const http = await api.defaultAdminClient();
+            let response = await http.putJson(urls.tenant(tenant.id), tenant);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user.id), user);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user2.id), user2);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user.id, claim.key), {
+                value: claim.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user.id, claim2.key), {
+                value: claim2.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user2.id, claim3.key), {
+                value: claim3.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+        });
+
+        it('Should not get second user claims when listing first user claims', async () => {
+            const http = await api.defaultAdminClient();
+            const response = await http.getJson(urls.adminClaims(tenant.id, user.id));
+            httpAssert.expectStatusCode(response, 200);
+            expect(response.body).to.be.eql([
+                claim,
+                claim2
+            ]);
+        });
+        
+        it('Should not get first user claims when listing second user claims', async () => {
+            const http = await api.defaultAdminClient();
+            const response = await http.getJson(urls.adminClaims(tenant.id, user2.id));
+            httpAssert.expectStatusCode(response, 200);
+            expect(response.body).to.be.eql([
+                claim3
+            ]);
+        });
+
+        it('Should not get second user claim when getting first user claim by second user claim key', async () => {
+            const http = await api.defaultAdminClient();
+            const response = await http.getJson(urls.adminClaim(tenant.id, user.id, claim3.key));
+            httpAssert.expectStatusCode(response, 404);
+        });
+    });
+    
+    describe('After registering claims for multiple users and updating one using admin api', async () => {
+        const user2 = data.randomUser();
+        const claim2 = data.randomClaim();
+        claim2.key = claim.key;
+
+        before(async () => {
+            await api.dropDatabase();
+            const http = await api.defaultAdminClient();
+            let response = await http.putJson(urls.tenant(tenant.id), tenant);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user.id), user);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user2.id), user2);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user.id, claim.key), {
+                value: claim.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user2.id, claim.key), {
+                value: claim.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user2.id, claim.key), {
+                value: claim2.value
+            });
+            httpAssert.expectStatusCode(response, 200);
+        });
+
+        assertClaim(tenant.id, user.id, claim);
+        assertClaim(tenant.id, user2.id, claim2);
+    });
+
+    describe('After registering claims for multiple users and deleting one using admin api', async () => {
+        const user2 = data.randomUser();
+
+        before(async () => {
+            await api.dropDatabase();
+            const http = await api.defaultAdminClient();
+            let response = await http.putJson(urls.tenant(tenant.id), tenant);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user.id), user);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminUser(tenant.id, user2.id), user2);
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user.id, claim.key), {
+                value: claim.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.putJson(urls.adminClaim(tenant.id, user2.id, claim.key), {
+                value: claim.value
+            });
+            httpAssert.expectStatusCode(response, 201);
+            response = await http.delete(urls.adminClaim(tenant.id, user2.id, claim.key));
+        });
+
+        assertClaim(tenant.id, user.id, claim);
+        assertNoClaim(tenant.id, user2.id, claim);
     });
 });
 
