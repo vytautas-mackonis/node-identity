5c2c15b08bb2d96f716bda9bc36b2840c8f39a0f
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-10 01:27:08 +0300

    renamed the resulting deb package for easier integration of deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 npm install
 gulp test
-node_modules/node-deb/node-deb --template-postrm tools/node-deb/postrm.template --install-strategy npm-install -- conf dist/app
+node_modules/node-deb/node-deb \
+    --template-postrm tools/node-deb/postrm.template \
+    --install-strategy npm-install \
+    -o node-identity.deb \
+    -- conf dist/app
