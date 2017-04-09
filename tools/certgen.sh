fa9e92cd4e76b6cd94b1375af529ca6b7201d108
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-10 01:27:08 +0300

    renamed the resulting deb package for easier integration of deployment

--- tools/certgen.sh
+++ tools/certgen.sh
@@ -2,3 +2,5 @@
 ssh-keygen -f key.rsa -t rsa -N ''
 rm key.rsa.pub
 openssl rsa -in key.rsa -pubout -outform PEM -out key.rsa.pub
+sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' key.rsa > key.rsa.tmp && mv key.rsa.tmp key.rsa
+sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' key.rsa.pub > key.rsa.pub.tmp && mv key.rsa.pub.tmp key.rsa.pub
