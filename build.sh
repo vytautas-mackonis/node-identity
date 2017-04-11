b012911517bc1d70d0207496de320bf8b59ad4e8
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-12 02:03:36 +0300

    not starting service with package installation

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ npm install
 gulp test
 node_modules/node-deb/node-deb \
     --template-postrm tools/node-deb/postrm.template \
+    --template-postinst tools/node-deb/postinst.template \
     --install-strategy npm-install \
     -o node-identity.deb \
     -- conf dist/app
